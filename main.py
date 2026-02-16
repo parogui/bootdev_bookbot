30e4d882cfacbed7a44b997fef1f60e674b1c3f2
Pablo Rodriguez <parodrig@redhat.com>
2026-02-16 21:05:28 +0100

    The bookbot now counts the words in the book

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+FRANKENSTEIN_BOOK="books/frankenstein.txt"
+
 def get_book_text(book):
     with open(book) as f:
         book_content = f.read()
         return(book_content)
-    
+
+def counting_words():
+    text=get_book_text(FRANKENSTEIN_BOOK).split(None,-1)
+    words_count=len(text)
+    print(f'Found {words_count} total words')
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    counting_words()
 
 main()
